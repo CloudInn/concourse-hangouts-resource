e27fed822144567e831bb5b2ce3210ce8c6524d2
Mohammad Allam <mallam@cloudinn.net>
2018-08-05 14:07:03 +0200

    Return array instead of object in check as concourse expects

--- assets/resource.py
+++ assets/resource.py
@@ -42,7 +42,7 @@ class GoogleChatNotifyResource:
 
     def check_res(self, source, params, workspace):
         """Return empty version to keep Concourse happy."""
-        return {"version": {}}
+        return [{"ref": ""}]
 
     def in_res(self, source, params, workspace):
         """Return empty version to keep Concourse happy."""
