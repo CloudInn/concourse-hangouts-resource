09731124d8da0bbe196e887128e99cfa05e10ce8
Nolan Cooper <nolancooper97@gmail.com>
2020-09-09 14:46:55 -0500

    Fix failure scenario test command

--- testing/test.sh
+++ testing/test.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -e
 cd ./assets
-cat ../testing/content.json | ./out
-cat ../testing/bad-content.json | ./out || true && false
+./out < ../testing/content.json
+if ./out < ../testing/bad-content.json ; then
+	false
+else
+	true
+fi
