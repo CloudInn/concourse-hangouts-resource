a22cf479cb69a1256e15c354e80fc123f7756a98
Nolan Cooper <nolancooper97@gmail.com>
2020-09-10 00:13:29 -0500

    Remove useless flags

--- testing/test.sh
+++ testing/test.sh
@@ -6,23 +6,23 @@ RESET='\e[0m'
 
 echo -e "${BR_YELLOW}Testing out sunny day with URL...${RESET}"
 ./out / < ../testing/with-url.json
-echo -e
+echo
 
 echo -e "${BR_YELLOW}Testing out sunny day without URL...${RESET}"
 ./out / < ../testing/no-url.json
-echo -e
+echo
 
 echo -e "${BR_YELLOW}Testing out sunny day URL not specified...${RESET}"
 ./out / < ../testing/basic.json
-echo -e
+echo
 
 echo -e "${BR_YELLOW}Testing in sunny day...${RESET}"
 ./in / < ../testing/basic.json
-echo -e
+echo
 
 echo -e "${BR_YELLOW}Testing check sunny day...${RESET}"
 ./check / < ../testing/basic.json
-echo -e
+echo
 
 echo -e "${BR_YELLOW}Testing out bad config...${RESET}"
 if ./out / < ../testing/bad-config.json ; then
@@ -30,7 +30,7 @@ if ./out / < ../testing/bad-config.json ; then
 else
 	true
 fi
-echo -e
+echo
 
 echo -e "${BR_YELLOW}Testing out bad url...${RESET}"
 if ./out / < ../testing/bad-url.json ; then
@@ -38,4 +38,4 @@ if ./out / < ../testing/bad-url.json ; then
 else
 	true
 fi
-echo -e
+echo
